441645cd266b88ab09910d419b78722799a1811f
haunteD7 <228pacan228aue@gmail.com>
2024-11-23 15:17:28 +0300

    added gitignore

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/User/Desktop/cpp unik/lab5
+# Install script for directory: C:/Users/User/Desktop/cpp/lab5
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/User/Desktop/cpp unik/lab5/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/User/Desktop/cpp/lab5/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
